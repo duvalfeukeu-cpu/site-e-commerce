ed3ca6da53f9038d2fa108c728f3f1c2e6759c7b
duvalfeukeu-cpu <ton-email-github@gmail.com>
2026-02-12 11:56:25 +0100

    Correction finale nom fonction

--- src/Services/productService.js
+++ src/Services/productService.js
@@ -1,12 +1,12 @@
 const BASE_URL = "https://fakestoreapi.com";
 
-// Fonction pour avoir tous les produits
-export const getAllProducts = async () => {
+// On garde uniquement "getProducts" car c'est ce que Accueil.jsx utilise
+export const getProducts = async () => {
   const response = await fetch(`${BASE_URL}/products`);
   return await response.json();
 };
 
-// Fonction pour avoir UN seul produit
+// Fonction pour avoir UN seul produit (utile pour la page Détails)
 export const getProductById = async (id) => {
   const response = await fetch(`${BASE_URL}/products/${id}`);
   return await response.json();
